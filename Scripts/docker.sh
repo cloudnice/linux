578f89f7cee4854c37db398103db6c34c060c743
Mehmet Denizli <154231505+foriinji@users.noreply.github.com>
2024-05-31 10:59:31 +0300

    Create docker.sh

---
+++ Scripts/docker.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker $USER   
+sudo usermod -aG docker jenkins
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
